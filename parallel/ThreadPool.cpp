ee8ea4c9d59af757bd971da61f8253cb0ee2bf5a
Italos Estilon <estilon@bitbucket.com>
2017-06-08 09:18:25 -0300

    thread pool

---
+++ parallel/ThreadPool.cpp
@@ -0,0 +1,104 @@
+//
+// Created by estilon on 06/06/17.
+//
+
+#include <random>
+#include <iostream>
+#include "ThreadPool.h"
+
+bool task::operator< (const task  &c) const {
+    return a < c.a;
+}
+
+ThreadPool::ThreadPool(int thread_number, function<vector<task>(task)> problem) {
+    cout << "fila esta " << pq.empty() << endl;
+    this->threadNumber = thread_number;
+    this->problem = problem;
+}
+
+void ThreadPool::enqueue(task call) {
+    std::unique_lock<std::mutex> lock(mutex_queue);
+    //cout << "colocando " << call.a << " na fila" << "\n";
+    pq.push(make_pair(call.a, call));
+
+    this->condition.notify_one();
+}
+
+task* ThreadPool::getTask() {
+    task *c = new task;
+    //cout << "tentando tirar da fila "  <<  " " <<  endl;
+    //cout << "algo" << endl;
+    if(!pq.empty()) {
+        pair<int, task> p = pq.top();
+        pq.pop();
+        *c = p.second;
+        //cout << "tirando da fila" << endl;
+    }else{
+        delete c;
+        c = nullptr;
+    }
+
+    return c;
+}
+
+
+void ThreadPool::run() {
+    for(int i = 0; i < this->threadNumber; i++){
+        busy.push_back(true);
+        thread_handles.push_back(thread(
+                [this, i]
+                {
+                    cout << "criando thread " << i << endl;
+                    for(;;)
+                    {
+                        cout << "thread " << i << " ficando desocupada" << endl;
+                        this->busy[i] = false;
+                        task* g = nullptr;
+                        {
+                            unique_lock<mutex> lock(this->mutex_queue);
+                            this->condition.wait(lock,
+                                                 [this] { return this->stop || !this->pq.empty(); });
+                            if (this->stop && this->pq.empty())
+                                return;
+
+                            this->busy[i] = true;
+
+                            g = this->getTask();
+                        }
+                        vector<task> calls = this->problem(*g);
+                        cout << "t " << i << " enfileirando " << calls.size() << " elementos" << endl;
+                        for(task c : calls){
+                            this->enqueue(c);
+                        }
+                        this->busy[i] = false;
+
+                        if(calls.empty()){
+                            bool flag = false;
+
+                            for (bool b : busy) {
+                                flag = b || flag;
+                            }
+                            if(!flag) {
+                                std::unique_lock<std::mutex> lock(mutex_queue);
+                                if(pq.empty())
+                                    this->stop = true;
+                            }
+
+                        }
+                    }
+                }));
+    }
+}
+
+ThreadPool::~ThreadPool() {
+    cout << "tentando parar" << endl;
+    {
+        std::unique_lock<std::mutex> lock(mutex_queue);
+        this->stop = true;
+    }
+    condition.notify_all();
+
+    for(std::thread &worker: thread_handles)
+        worker.join();
+}
+
---
+++ parallel/ThreadPool.h
@@ -0,0 +1,49 @@
+//
+// Created by estilon on 06/06/17.
+//
+
+#include <vector>
+#include <queue>
+#include <pthread.h>
+#include <functional>
+#include <thread>
+#include <condition_variable>
+
+#ifndef POOLTHREAD_THREADPOOL_H
+#define POOLTHREAD_THREADPOOL_H
+
+
+class task{
+public:
+    int a;
+
+    bool operator<(const task &) const;
+};
+
+using namespace std;
+
+class ThreadPool {
+
+private:
+    priority_queue < pair< int, task>, vector< pair < int, task> > > pq;
+    vector<thread> thread_handles;
+    condition_variable condition;
+    vector<bool> busy;
+    function<vector<task>(task)> problem;
+    int threadNumber;
+
+    mutex mutex_queue;
+
+    bool stop = false;
+
+public:
+
+    ThreadPool(int thread_number, std::function<vector<task>(task)> problem);
+    ~ThreadPool();
+    void run();
+    task* getTask();
+    void enqueue(task call);
+};
+
+
+#endif //POOLTHREAD_THREADPOOL_H
